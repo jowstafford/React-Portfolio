05e28e72e0708cdb52248e3722a5249e96030646
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-04 14:40:39 -0500

    Added fontawesome icons and my personal information. added style to spans and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
-import "./Navbar.scss";
+import "./topbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-export default function Topbar() {
+export default function Navbar() {
   return (
     <div class="navbar">
-      <div class="container">
+      <div class="wrapper">
         <div class="left">
           <a href="#intro" class="logo">
-            <img src="images/JS_Icon.png" alt="" class="jsLogo" />
+            <img src="assets/JS_Icon.png" alt="" class="jsLogo"/>
           </a>
+          <div class="itemContainer">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <span>+1 903 744 9700</span>
+          </div>
+          <div class="itemContainer">
+            <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            <span>jordanstafforddev@gmail.com</span>
+          </div>
         </div>
       </div>
     </div>
